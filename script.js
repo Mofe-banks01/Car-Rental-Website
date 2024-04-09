b3af9ea4f51995aded50cec8b9dc494dfbcbe8f3
Mofe-banks01 <bankoleadedotun16@gmail.com>
2024-04-09 22:47:56 +0100

    Addition of Mobie nav, Creation of Products Array

---
+++ script.js
@@ -0,0 +1,60 @@
+// Implement Booking functionality
+
+var typed = new Typed("#element", {
+  strings: [
+    "Efficient Car Distribution System",
+    "Awesome Customer Service",
+    "Affordable Rentals",
+    "Government Certified",
+    "Used my Major Tech Companies",
+  ],
+  typeSpeed: 50,
+  backDelay: 215,
+  backSpeed: 30,
+});
+
+const firmProductData = [
+  {
+    name: "Honda XCT",
+    model: "KC1509",
+    price: 170,
+    img: "Images/product--1.jpg",
+    productAvailability: true,
+    URL: "link to the product",
+  },
+  {
+    name: "Mazda CX9",
+    model: "MP8700",
+    price: 170,
+    img: "Images/product--2.jpg",
+    productAvailability: true,
+    URL: "link to the product",
+  },
+  {
+    name: "Toyota Seron",
+    model: "SR5654",
+    price: 170,
+    img: "Images/product--3.jpg",
+    productAvailability: false,
+    URL: "link to the product",
+  },
+  {
+    name: "Volks Golf",
+    model: "GF3665",
+    price: 170,
+    img: "Images/product--1.jpg",
+    productAvailability: true,
+    URL: "link to the first product",
+  },
+];
+
+const openMobileNavbtn = document.querySelector(".bars");
+const mobileNav = document.querySelector(".mobile--nav");
+
+function openMobNav() {
+  mobileNav.classList.toggle("hideNav");
+}
+
+openMobileNavbtn.addEventListener("click", openMobNav);
+
+// console.log(firmProductData);
